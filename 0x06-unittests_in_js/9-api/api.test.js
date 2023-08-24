e3e232c3f7557ca1da0e6553edd60fb19ffbd880
Chidi <ajaerohc@gmail.com>
2023-08-24 23:30:47 +0100

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,30 @@
+const {describe, it} = require("mocha");
+
+const request = require("request");
+
+const expect = require("chai").expect;
+
+describe("Index page", function() {
+    const option = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", function(done) {
+	request(option, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
+    });
+    it("check correct content", function(done) {
+	request(option, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
+    });
+    it("check correct content length", function(done) {
+	request(option, function(err, res, body) {
+	    expect(res.headers['content-length']).to.equal('29');
+	    done();
+	});
+    });
+});

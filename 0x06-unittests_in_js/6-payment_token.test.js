6b1d367e1ee1925fda7c40c180adfc84ce300930
Chidi <ajaerohc@gmail.com>
2023-08-24 23:07:36 +0100

    supporting async testing

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const {describe, it} = require("mocha");
+const getPaymentTokenFromAPI = require("./6-payment_token");
+const expect = require("chai").expect;
+
+describe("getPaymentTokenFromAPI", function() {
+    it("Async testing with done callback", function(done) {
+	getPaymentTokenFromAPI(true)
+	    .then((data) => {
+		expect(data).to.have.property('data');
+		done();
+	    });
+    });
+});

c7c5b6da5c9b345abb3e79ba5048c43b864b9783
Chidi <ajaerohc@gmail.com>
2023-08-24 13:04:33 +0100

    Spies are a useful wrapper

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,18 @@
+const {describe, it} = require("mocha");
+
+const sinon = require("sinon");
+
+const sendPaymentRequestToApi = require("./3-payment");
+const assert = require("assert");
+const Utils = require("./utils");
+
+describe("sendPaymentRequestToApi", function() {
+    it("check that Utils.calculateNumber was called once", function() {
+	const spy = sinon.spy(Utils, "calculateNumber");
+
+	sendPaymentRequestToApi(50, 24.52);
+
+	assert(spy.calledOnce);
+	spy.restore();
+    });
+});
